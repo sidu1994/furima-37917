b9f092c83059147384b141326e6cdce01fadbe17
sidu1994 <106261319+sidu1994@users.noreply.github.com>
2022-08-22 00:46:24 +0900

    整数だけになるように修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   validates :load_id ,numericality: { other_than: 1 , message: "can't be blank"} 
   validates :area_id ,numericality: { other_than: 1 , message: "can't be blank"} 
   validates :deadline_id ,numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :price ,presence: true ,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price ,presence: true ,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , only_integer: true}
 
   
 
