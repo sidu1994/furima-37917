7bbbde277c2742feaa938d3b02ff3043f6294cc9
sidu1994 <106261319+sidu1994@users.noreply.github.com>
2022-08-30 00:49:19 +0900

    変更の保存及び編集ページへの遷移をさせない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,10 +20,23 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+ 
   end
 
   def edit
     @item = Item.find(params[:id])
+    if current_user != @item.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+    redirect_to root_path
+    else
+      render :edit
+   end
   end
 
   private
