96f61f31c57194cf776d55b88c5804940c4b7e5e
sidu1994 <106261319+sidu1994@users.noreply.github.com>
2022-09-05 00:09:30 +0900

    購入画面にて購入商品情報の表示

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -1,6 +1,24 @@
 class ShippingsController < ApplicationController
 
   def index
+  @item = Item.find(params[:item_id])
+
   end
-  
+
+  def create
+    @shipping = Shipping.new(shippng_params)
+    if @shipping.valid?
+      @shipping.save
+      return redirect_to root_path
+    else
+      render shipping_path
+    end
+  end
+
+  private
+
+  def shipping_params
+    params.require(:shipping).permit(:post_code,:area_id,:municipality,:address,:building,:phone_number).merge(user_id: current_user.id, purchase_id: purchase.id)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "items#index"
-  resources :items 
-  resources :shippings, only: [:index]
+  resources :items do
+    resources :shippings, only: [:index, :create]
+  end
 
 end
